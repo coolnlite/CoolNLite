adbe5497efa9bf9eab659954f2ac758f24b93077
coolnlite <mtauto.marketing@gmail.com>
2022-05-28 11:20:14 +0700

    fix ui

--- admin/modules/add_data.php
+++ admin/modules/add_data.php
@@ -8,8 +8,13 @@
            isset($_POST['url']) && isset($_POST['title']) 
             && isset($_POST['thumnail']) && isset($_POST['description'])
             && isset($_POST['content']) && isset($_POST['radio'] )
-       ){
-
+       )
+       {
+        $url = mysqli_real_escape_string($conn, $_POST['url']);
+        $title = mysqli_real_escape_string($conn, $_POST['title']);
+        $description = mysqli_real_escape_string($conn, $_POST['description']);
+        $content = mysqli_real_escape_string($conn, $_POST['content']);
+        $radio = mysqli_real_escape_string($conn, $_POST['radio']);
        }
     }
 ?>
