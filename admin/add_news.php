5f3f5ccdaf763fc0b6a76fb6b15263608527107a
coolnlite <mtauto.marketing@gmail.com>
2022-05-29 15:54:42 +0700

    fix ui

--- admin/add_news.php
+++ admin/add_news.php
@@ -19,8 +19,6 @@
     <!-- fontawesome -->
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.6.0/jq-3.6.0/jszip-2.5.0/dt-1.12.1/b-2.2.3/b-colvis-2.2.3/b-html5-2.2.3/b-print-2.2.3/datatables.min.css"/>
 
     <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -51,9 +49,7 @@
         filebrowserUploadUrl: 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Files'
         } );
     </script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.6.0/jq-3.6.0/jszip-2.5.0/dt-1.12.1/b-2.2.3/b-colvis-2.2.3/b-html5-2.2.3/b-print-2.2.3/datatables.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
     <script>
        
@@ -72,11 +68,13 @@
             cache: false,
             processData:false,
             success: function(data) {
-              if (data.status != 0) {
+            var data = JSON.parse(data);
+            console.log(data);
+            if (data.status != 0) {
                 window.location('<?php print $DOMAIN.'news.php'?>');
-              }else{
+            }else{
                 alert(data.message);
-              }
+            }
             }
         })
        
