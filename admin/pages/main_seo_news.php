93d9d8db540349582b6e81fd8fea8fdc2454b391
coolnlite <mtauto.marketing@gmail.com>
2022-06-02 09:17:45 +0700

    fix seo

---
+++ admin/pages/main_seo_news.php
@@ -0,0 +1,24 @@
+<div  style="width: 94%; margin : 0 auto">
+  <h2>SEO Cho Bài Viết</h2>
+
+  <ul class="nav nav-tabs">
+    <li class="active"><a data-toggle="tab" href="#main">Home</a></li>
+    <li><a data-toggle="tab" href="#fb">Facebook</a></li>
+    <li><a data-toggle="tab" href="#tw">Twitter</a></li>
+  </ul>
+
+  <div class="tab-content">
+    <div id="main" class="tab-pane fade in active">
+      <h3>SEO Chính</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div id="fb" class="tab-pane fade">
+      <h3>SEO facebook</h3>
+      <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </div>
+    <div id="tw" class="tab-pane fade">
+      <h3>SEO twitter</h3>
+      <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    </div>
+  </div>
+</div>
--- admin/seo_news.php
+++ admin/seo_news.php
@@ -34,7 +34,9 @@
     <link href="https://kit-pro.fontawesome.com/releases/v5.15.1/css/pro.min.css" rel="stylesheet" />
     <!-- fontawesome -->
     <!-- Styles -->
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
     <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -57,8 +59,6 @@
 
    <!-- Script -->
     <script src="./js/main.js"></script>
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
 </body>
 
 </html>
