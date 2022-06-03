34e7f9982b8f31e085e863f7ef208931faba9350
coolnlite <mtauto.marketing@gmail.com>
2022-06-03 11:10:30 +0700

    fix bug

--- admin/pages/main_agency.php
+++ admin/pages/main_agency.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
     <div class="row d-flex justify-content-end ">
-    <a type="button" class="btn btn-outline-success mb-3 mr-5">Thêm đại lý</a>
+    <a type="button" data-toggle="modal" data-target="#addAgency" class="btn btn-outline-success mb-3 mr-5">Thêm đại lý</a>
       <div style="width: 94%; margin : 0 auto">
         <div class="row">
           <div class="col-xl-12">
@@ -23,4 +23,50 @@
       </div>
     </div>
   </div>
+
+  <div class="modal fade" id="addAgency" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Thêm đại lý</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body" id="modal-body-view">
+        <form id="fAddAgency" class="needs-validation" enctype="multipart/form-data" novalidate>
+
+        <div class="form-group">
+        <label for="img">Ảnh đại lý :</label>
+        <input type="file" class="form-control" name="img" id="img" required>
+        <div class="invalid-feedback">Vui lòng nhập ảnh đại đại lý</div>
+        </div>
+
+        <div class="form-group">
+        <label for="url">Đường dẫn :</label><label class="text-danger">(Mẫu : vo-dong-thai-dep-trai-ahihi)</label>
+        <input type="text" class="form-control" id="url" placeholder="Nhập đường dẫn bài viết" name="url" required>
+        <div class="invalid-feedback">Vui lòng nhập đường dẫn bài viết</div>
+        </div>
+
+        <div class="form-group">
+        <label for="title">Tiêu đề :</label> <label class="text-danger">không quá 200 ký tự</label>
+        <input type="text" class="form-control" id="title" placeholder="Nhập tiêu đề bài viết" name="title" required>
+        <div class="invalid-feedback">Vui lòng nhập tiêu đề bài viết</div>
+        </div>
+
+        <div class="form-group">
+        <label for="description">Mô tả :</label> <label class="text-danger">không quá 300 ký tự</label>
+        <input type="text" class="form-control" id="description" placeholder="Nhập mô tả bài viết" name="description" required>
+        <div class="invalid-feedback">Vui lòng nhập mô tả bài viết</div>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Thêm</button>
+    </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+      </div>
+    </div>
+  </div>
+</div>
 
