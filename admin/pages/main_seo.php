ed16b84f830ad61bcb1efc836fded2631c72335e
coolnlite <mtauto.marketing@gmail.com>
2022-06-03 16:47:02 +0700

    fix bug

---
+++ admin/pages/main_seo.php
@@ -0,0 +1,52 @@
+<div style="margin: 0 auto; width : 94%">
+<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#home" role="tab">Trang Chủ</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#premier" role="tab">Premier Series</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#titanx" role="tab">Titan X Series</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#news" role="tab">Tin Tức</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#about" role="tab">Chúng Tôi</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#agency" role="tab">Đại Lý</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#tag" role="tab">Từ Khóa</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#search" role="tab">Tìm Kiếm</a>
+  </li>
+</ul>
+<div class="tab-content" id="pills-tabContent">
+  <div class="tab-pane fade show active" id="home" role="tabpanel">
+
+  </div>
+  <div class="tab-pane fade" id="premier" role="tabpanel">
+
+  </div>
+  <div class="tab-pane fade" id="titanx" role="tabpanel">
+
+  </div>
+  <div class="tab-pane fade" id="news" role="tabpanel">
+
+  </div>
+  <div class="tab-pane fade" id="about" role="tabpanel">
+
+  </div>
+  <div class="tab-pane fade" id="agency" role="tabpanel">
+
+  </div>
+  <div class="tab-pane fade" id="tag" role="tabpanel">
+
+  </div>
+  <div class="tab-pane fade" id="search" role="tabpanel"></div>
+</div>
+</div>
