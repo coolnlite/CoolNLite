f5b19d33dd936d183c5a1845797e4891ca1dedbb
coolnlite <mtauto.marketing@gmail.com>
2022-06-04 10:13:17 +0700

    fix bug

---
+++ admin/pages/main_account.php
@@ -0,0 +1,72 @@
+<div style="margin: 0 auto; width : 94%">
+    <div class="rounded bg-white mb-5 mt-2">
+        <div class="row">
+            <div class="col-xl-3 border-right">
+                <div class="d-flex flex-column align-items-center text-center">
+                    <img class="rounded-circle" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
+                    <span class="font-weight-bold">Võ Đông Thái</span>
+                    <span class="text-black-50">Administrator</span>
+                    <a class="btn btn-outline-primary mt-1">Thay đổi ảnh đại diện</a>
+                </div>
+            </div>
+            <div class="col-xl-9 border-right">
+            <h5>Thông Tin Tài Khoản</h5>
+            <form class="needs-validation" novalidate>
+                <div class="form-row">
+                    <div class="col-xl-6 mb-3">
+                    <label for="validationCustomUsername">Username</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                        </div>
+                        <input type="text" class="form-control" id="validationCustomUsername" placeholder="Tên tài khoản" aria-describedby="inputGroupPrepend" required>
+                        <div class="invalid-feedback">
+                            Vui lòng số tài khoản
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="col-xl-6 mb-3">
+                    <label for="validationCustom02">Email</label>
+                    <input type="email" class="form-control" id="validationCustom02" placeholder="Email" value="" required>
+                    <div class="invalid-feedback">
+                       Vui lòng nhập email
+                    </div>
+                    </div>
+
+                </div>
+                <div class="form-row">
+                    <div class="col-xl-6 mb-3">
+                    <label for="validationCustom04">Vị trí</label>
+                    <input type="text" class="form-control" id="validationCustom04" placeholder="Vị trí" required>
+                    <div class="invalid-feedback">
+                        Please provide a valid state.
+                    </div>
+                    </div>
+                    <div class="col-xl-6 mb-3">
+                    <label for="validationCustom05">Tên đầy đủ</label>
+                    <input type="text" class="form-control" id="validationCustom05" placeholder="Tên đầy đủ" required>
+                    <div class="invalid-feedback">
+                        Please provide a valid zip.
+                    </div>
+                    </div>
+                </div>
+                <div class="form-row mb-3">
+                    <div class="col-xl-12 mb-12">
+                    <label for="validationCustom03">Trạng thái</label>
+                    <input type="text" class="form-control" id="validationCustom03" placeholder="" required>
+                    <div class="invalid-feedback">
+                        Please provide a valid city.
+                    </div>
+                    </div>
+                </div>
+                <div class="form-row mb-3 d-flex justify-content-center">
+                    <button type="button" class="btn btn-success">Thay đổi thông tin</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+</div>
