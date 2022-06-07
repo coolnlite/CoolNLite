ca8701e27f05080ec857b68782e2c19d2d47ed14
coolnlite <mtauto.marketing@gmail.com>
2022-06-07 16:56:36 +0700

    fix bug

--- admin/modules/fetch_data.php
+++ admin/modules/fetch_data.php
@@ -587,4 +587,83 @@ if(isset($_POST['footer'])){
 
 }
 
+//Hiện thị menu
+
+if(isset($_POST['sidebar'])){
+    $output= array();
+    $sql = "SELECT * FROM `sidebar` ";
+
+    $totalQuery = mysqli_query($conn,$sql);
+    $total_all_rows = mysqli_num_rows($totalQuery);
+   
+    $columns = array(
+        0 => 'id',
+        1 => 'url',
+        2 => 'icon',
+        3 => 'name',
+        4 => 'position',
+        5 => 'time'
+    );
+    
+    if(isset($_POST['search']['value']))
+    {
+        $search_value = $_POST['search']['value'];
+        $sql .= " WHERE `url` like '%".$search_value."%'";
+        $sql .= " OR `name` like '%".$search_value."%'";
+        $sql .= " OR `position` like '%".$search_value."%'";
+    }
+
+    if(isset($_POST['order']))
+    {
+        $column_name = $_POST['order'][0]['column'];
+        $order = $_POST['order'][0]['dir'];
+        $sql .= " ORDER BY ".$columns[$column_name]." ".$order."";
+    }
+    else
+    {
+        $sql .= " ORDER BY `position` asc";
+    }
+
+    if($_POST['length'] != -1)
+    {
+        $start = $_POST['start'];
+        $length = $_POST['length'];
+        $sql .= " LIMIT  ".$start.", ".$length;
+    }	
+   
+    $query = mysqli_query($conn,$sql);
+    $count_rows = mysqli_num_rows($query);
+    $data = array();
+
+    while($row = mysqli_fetch_assoc($query))
+    {
+        $sub_array = array();
+        $sub_array[] = $row['id'];
+        $sub_array[] = $row['name'];
+        $sub_array[] = $row['url'];
+        $sub_array[] = $row['position'];
+        $sub_array[] = facebook_time_ago($row['time']);
+        $sub_array[] = 
+        '
+        <a title="Xóa" href="javascript:void();" data-id="'.$row['id'].'"  class="btn btn-danger btn-sm deleteBtn" >
+        <i class="fas fa-trash-alt"></i>
+        </a>
+        <a title="Sửa" data-toggle="modal" data-target="#editMenu" href="javascript:void();" data-id="'.$row['id'].'"
+        class="btn btn-warning btn-sm editBtn" >
+        <i class="fas fa-user-edit"></i>
+        </a>
+        ';
+        $data[] = $sub_array;
+    }
+
+    $output = array(
+        'draw'=> intval($_POST['draw']),
+        'recordsTotal' =>$count_rows ,
+        'recordsFiltered'=>   $total_all_rows,
+        'data'=>$data,
+    );
+    echo  json_encode($output);
+
+}
+
 ?>
