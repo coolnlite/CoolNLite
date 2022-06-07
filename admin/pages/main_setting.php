ec3c93b4ce506ed33a3414d41f15e28bacfe9c3e
coolnlite <mtauto.marketing@gmail.com>
2022-06-07 16:17:21 +0700

    fix bug

--- admin/pages/main_setting.php
+++ admin/pages/main_setting.php
@@ -1,6 +1,8 @@
 <div class="container-fluid">
     <div class="row">
       <div style="width: 94%; margin : 0 auto">
+      <!-- MENU -->
+
       <div class="d-flex justify-content-between align-items-center pb-3">
         <span class="text-center h5">MENU</span>
         <button type="button" class="btn btn-outline-success">Thêm menu</button>
@@ -10,7 +12,7 @@
             <table id="example" class="table table-striped table-bordered" style="width: 100%;">
               <thead>
                 <th>Id</th>
-                <th>Tên menu</th>
+                <th>Tên</th>
                 <th>Đường dẫn</th>
                 <th>Vị trí</th>
                 <th>Thời gian tạo</th>
@@ -22,6 +24,58 @@
           </div>
           <div class="col-md-2"></div>
         </div>
+
+        <!-- FOOTER -->
+
+        <div class="d-flex justify-content-between align-items-center pb-3">
+        <span class="text-center h5">FOOTER</span>
+        </div>
+          <div class="row">
+            <div class="col-xl-12">
+              <table id="example" class="table table-striped table-bordered" style="width: 100%;">
+                <thead>
+                  <th>Id</th>
+                  <th>Copyright</th>
+                  <th>Địa chỉ</th>
+                  <th>Số điện thoại</th>
+                  <th>Email</th>
+                  <th>Tiêu đề</th>
+                  <th>Mô tả</th>
+                  <th>Thời gian cập nhật</th>
+                  <th>Thao tác</th>
+                </thead>
+                <tbody>
+                </tbody>
+              </table>
+            </div>
+            <div class="col-md-2"></div>
+          </div>
+
+          <!-- SIDEBAR -->
+
+        <div class="d-flex justify-content-between align-items-center pb-3">
+        <span class="text-center h5">SIDEBAR</span>
+        </div>
+          <div class="row">
+            <div class="col-xl-12">
+              <table id="example" class="table table-striped table-bordered" style="width: 100%;">
+                <thead>
+                <th>Id</th>
+                <th>Đường dẫn</th>
+                <th>Icons</th>
+                <th>Tên</th>
+                <th>Vị trí</th>
+                <th>Thời gian tạo</th>
+                <th>Thao tác</th>
+                </thead>
+                <tbody>
+                </tbody>
+              </table>
+            </div>
+            <div class="col-md-2"></div>
+          </div>
+
+
       </div>
     </div>
   </div>
