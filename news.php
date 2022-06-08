66d0cf77850145c2c28c18d72d5c2169bf789dc0
coolnlite <mtauto.marketing@gmail.com>
2022-06-08 17:04:18 +0700

    fix bug

--- news.php
+++ news.php
@@ -2,6 +2,8 @@
     require_once('./config/config.php');
     require_once('./config/dbhelper.php');
     require_once('./admin/modules/function.php');
+    $base_url = 'http://localhost/CoolNLite';
+    date_default_timezone_set('Asia/Ho_Chi_Minh');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,11 +11,54 @@
       <meta charset="UTF-8" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <?php
+        $sql = "SELECT * FROM `seo_pages` WHERE `id` = 4";
+        $seo_pages = executeResult($sql);
+        foreach($seo_pages as $sp){
+            
+    ?>
+      <title><?php print $sp['title']?></title>
+      <meta name="description" content="<?php print $sp['description']?>"/>
+      <meta name="keywords" content="<?php print $sp['keyword']?>"/>
+      <meta name="robots" content="noarchive,index,follow"/>
+      <meta http-equiv=”content-language” content=”vi” />
+      <meta name="copyright" content="COOL N LITE"/>
+      <meta name="author" content="COOL N LITE"/>
+      <meta name="robots" content="noarchive,index,follow"/>
+      <meta name="googlebot" content="noarchive"/>
+      <meta name="geo.placename" content="Ho Chi Minh, Viet Nam"/>
+      <meta name="geo.region" content="VN-HCM"/>
+      <meta name="geo.position" content="106.0125,107.0110"/>
+      <meta name="ICBM" content="106.0125, 107.0110"/>
+      <meta name="revisit-after" content="days"/>
+      <!-- meta main -->
+      <!-- FACEBOOK -->
+      <meta property="og:site_name" content="coolnlite.vn"/>
+      <meta property="og:rich_attachment" content="true"/>
+      <meta property="og:type" content="article"/>
+      <meta property="article:publisher" content="<?php print $sp['link_fb']?>"/>
+      <meta property="og:url" itemprop="url" content="<?php print getCurrentPageURL() ?>"/>
+      <meta property="og:image" itemprop="thumbnailUrl" content="<?php print $base_url.$sp['img_fb']?>"/>
+      <meta property="og:image:width" content="800"/>
+      <meta property="og:image:height" content="354"/>
+      <meta content="<?php print $sp['title_fb']?>" itemprop="headline" property="og:title"/>
+      <meta content="<?php print $sp['description_fb']?>" itemprop="description" property="og:description"/>
+      <meta property="article:tag" content="<?php print $sp['keyword_fb']?>"/>
+      <!-- FACEBOOK -->
+      <!-- Twitter Card -->
+      <meta name="twitter:card" value="summary"/>
+      <meta name="twitter:url" content="<?php print getCurrentPageURL() ?>"/>
+      <meta name="twitter:title" content="<?php print $sp['title_tw']?>"/>
+      <meta name="twitter:description" content="<?php print $sp['description_tw']?>"/>
+      <meta name="twitter:image" content="<?php print $base_url.$sp['img_tw']?>"/>
+      <meta name="twitter:site" content="@COOLNLITE"/>
+      <meta name="twitter:creator" content="@COOLNLITE"/>
+      <!-- End Twitter Card -->
+      <?php 
+            }//Vòng lặp seo
+      ?>
       <!-- fontawesome -->
-      <link
-         href="https://kit-pro.fontawesome.com/releases/v5.15.1/css/pro.min.css"
-         rel="stylesheet"
-         />
+      <link href="https://kit-pro.fontawesome.com/releases/v5.15.1/css/pro.min.css" rel="stylesheet" />
       <!-- fontawesome -->
       <!-- css -->
       <link rel="icon" sizes="32x32" href="./shared/img/icon.png">
