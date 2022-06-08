46c100339a5ab50631e89e220bc0537b74b22018
coolnlite <mtauto.marketing@gmail.com>
2022-06-08 10:25:33 +0700

    fix bug

--- admin/modules/edit_data.php
+++ admin/modules/edit_data.php
@@ -2,6 +2,8 @@
  require_once('../../config/config.php');
  require_once('../../config/dbhelper.php');
 
+//Chỉnh sửa từ khóa
+
 if(isset($_POST['id_key']) && isset($_POST['name_key'])){
     $id_key = $_POST['id_key'];
     $name_key = mysqli_real_escape_string($conn, $_POST['name_key']);
@@ -2028,5 +2030,30 @@ if(!empty($_FILES['img_tw']) && $_FILES['img_tw']['error'] == 0){
             exit();
         }
     }
+
+    //Chỉnh sửa menu
+
+    if(isset($_POST['id_key']) && isset($_POST['name_key'])){
+        $id_key = $_POST['id_key'];
+        $name_key = mysqli_real_escape_string($conn, $_POST['name_key']);
+        $time = date('Y-m-d H:i:s');
+    
+        $sql = "UPDATE `keyword` SET `name` = '$name_key', `time` = '$time' WHERE `id` = '$id_key'";
+        $result = mysqli_query($conn,$sql);
+        if($result == true){
+            echo json_encode(array(
+                'status' => 1,
+                'message' => 'Cập nhật từ khóa thành công'
+            ));
+            exit();
+        }else{
+            echo json_encode(array(
+                'status' => 0,
+                'message' => 'Cập nhật từ khóa thất bại'
+            ));
+            exit();
+        }
+    }
+
 
  ?>
