b165d7dc34e04efbbb03dc6ef3b4f04c5b8fb7a0
coolnlite <mtauto.marketing@gmail.com>
2022-06-17 17:05:24 +0700

    fix bug

--- admin/modules/password_reset_code.php
+++ admin/modules/password_reset_code.php
@@ -2,4 +2,7 @@
     require_once('../../config/config.php');
     require_once('../../config/dbhelper.php');
 
+    if(isset($_POST['reset_password']) && !empty($_POST['email'])){
+        
+    }
 ?>
