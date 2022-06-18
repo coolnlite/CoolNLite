e4a2cd4fd1fe58e3b69e275b88d4e52c41e8c514
coolnlite <mtauto.marketing@gmail.com>
2022-06-18 11:30:20 +0700

    fix bug

--- admin/modules/password_reset_code.php
+++ admin/modules/password_reset_code.php
@@ -22,8 +22,8 @@
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
             $mail->Username   = 'damlongcaca@gmail.com';                     //SMTP username
-            $mail->Password   = '***';                               //SMTP password
-            $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
+            $mail->Password   = '';                               //SMTP password
+            $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
             $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
