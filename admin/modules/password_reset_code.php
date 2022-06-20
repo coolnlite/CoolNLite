10afb5d5dcff13140a08883421489d3b1d6fc1ff
coolnlite <mtauto.marketing@gmail.com>
2022-06-20 09:09:37 +0700

    fix bug

--- admin/modules/password_reset_code.php
+++ admin/modules/password_reset_code.php
@@ -5,7 +5,7 @@
 
     include("../PHPMailer/src/PHPMailer.php");
     include("../PHPMailer/src/Exception.php");
-    // include("../PHPMailer/src/OAuth.php");
+    include("../PHPMailer/src/OAuth.php");
     include("../PHPMailer/src/POP3.php");
     include("../PHPMailer/src/SMTP.php");
      
