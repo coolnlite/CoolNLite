348a51fbffcb08f9d5d92f5c76a37667730e18a7
coolnlite <mtauto.marketing@gmail.com>
2022-06-24 13:48:49 +0700

    fix bug

--- admin/gallery_img.php
+++ admin/gallery_img.php
@@ -4,6 +4,13 @@
     require_once('./modules/permision.php');
     require_once('../config/config.php');
     require_once('../config/dbhelper.php'); 
+
+    if(isset($_GET['id']) && !empty($_GET['id'])){
+      $id = $_GET['id'] = !"" ? mysqli_real_escape_string($conn, $_GET['id']) : '';
+    }else{
+      require_once('./error_404.php');
+      exit();
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,15 +19,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thư Viện</title>
+    <title>CHỈNH SỬA BÀI VIẾT</title>
     <link rel="icon" sizes="32x32" href="../shared/img/icon.png">
     <!-- fontawesome -->
     <link href="https://kit-pro.fontawesome.com/releases/v5.15.1/css/pro.min.css" rel="stylesheet" />
     <!-- fontawesome -->
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/css/bootstrap.min.css"/>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4-4.6.0/jq-3.6.0/jszip-2.5.0/dt-1.12.1/b-2.2.3/b-colvis-2.2.3/b-html5-2.2.3/b-print-2.2.3/datatables.min.css"/>
 
     <link rel="stylesheet" href="./css/style.css">
 </head>
@@ -36,22 +41,19 @@
         <div class="main">
            <?php
             require_once('./pages/topbar.php');
-            if($permission != 2){
-              require_once('./pages/main_premision.php');
-            }else{
-              require_once('./pages/main_gallery_img.php');
-            }
+            if($permission == 0){
+                require_once('./pages/main_premision.php');
+              }else{
+                require_once('./pages/main_edit_news.php');
+              }
            ?>
         </div>
     </div>
 
    <!-- Script -->
     <script src="./js/main.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4-4.6.0/jq-3.6.0/jszip-2.5.0/dt-1.12.1/b-2.2.3/b-colvis-2.2.3/b-html5-2.2.3/b-print-2.2.3/datatables.min.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js"></script>
- 
 </body>
 
 </html>
