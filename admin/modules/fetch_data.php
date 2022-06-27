dd6ff7b20fde6c682276efd3710b87fbc0a42e98
coolnlite <mtauto.marketing@gmail.com>
2022-06-27 14:42:43 +0700

    fix bug

--- admin/modules/fetch_data.php
+++ admin/modules/fetch_data.php
@@ -755,16 +755,15 @@ if(isset($_POST['gallery_img']) && !empty($_POST['id_gallery_img'])){
     $id_gallery_img = mysqli_real_escape_string($conn, $_POST['id_gallery_img']);
 
     $output= array();
-    $sql = "SELECT * FROM `gallery_img` WHERE `id_gallery` = $id_gallery_img";
+    $sql = "SELECT * FROM `gallery_img` WHERE `id_gallery` = '$id_gallery_img'";
 
     $totalQuery = mysqli_query($conn,$sql);
     $total_all_rows = mysqli_num_rows($totalQuery);
    
     $columns = array(
         0 => 'id',
-        1 => 'id_gallery',
-        2 => 'image',
-        3 => 'time'
+        1 => 'image',
+        2 => 'time'
     );
     
     if(isset($_POST['search']['value']))
@@ -799,7 +798,6 @@ if(isset($_POST['gallery_img']) && !empty($_POST['id_gallery_img'])){
     {
         $sub_array = array();
         $sub_array[] = $row['id'];
-        $sub_array[] = $row['id_gallery'];
         $sub_array[] = '<img src="'.$base_url.$row['image'].'" alt="ảnh đại diện">';
         $sub_array[] = facebook_time_ago($row['time']);
         $sub_array[] = 
