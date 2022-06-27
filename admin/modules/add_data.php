fb6fb81fb26694119048a4e9520802c3bfaac481
coolnlite <mtauto.marketing@gmail.com>
2022-06-27 12:53:08 +0700

    fix bug

--- admin/modules/add_data.php
+++ admin/modules/add_data.php
@@ -651,14 +651,14 @@ if(!empty($_POST['id_gallery'])){
                         $sqlVal = "('".$id_gallery."' ,'".$gallery_img."', '".$time."')";
 
                     }else{
-                        $message .= '<p class="text-danger text-center" id="notify_move">'.$filename.' uploads không thành công</p>';
+                        $message .= '<p class="text-danger text-center" >'.$filename.' uploads không thành công</p>';
                     }
                 }else{
-                    $message .= '<p class="text-danger text-center" id="notify_move">'.$filename.' uploads không thành công .Vui lòng chọn ảnh có dung lượng nhỏ hơn hoặc bằng 2MB</p>';
+                    $message .= '<p class="text-danger text-center" >'.$filename.' uploads không thành công .Vui lòng chọn ảnh có dung lượng nhỏ hơn hoặc bằng 2MB</p>';
 
                 }
             }else{
-                $message .= '<p class="text-danger text-center" id="notify_move">'.$filename.' uploads không thành công .Vui lòng chọn ảnh đuôi là jpg, jpeg, png, bmp, gif</p>';
+                $message .= '<p class="text-danger text-center" >'.$filename.' uploads không thành công .Vui lòng chọn ảnh đuôi là jpg, jpeg, png, bmp, gif</p>';
             }
 
             //Thêm data vào db
@@ -666,9 +666,9 @@ if(!empty($_POST['id_gallery'])){
                 $insert = $conn -> query("INSERT INTO `gallery_img` (`id_gallery`, `image`, `time`) 
                 VALUES $sqlVal");
                 if($insert) {
-                    $message .= '<p class="text-success text-center" id="notify_move">Uploads ảnh thành công</p>'; 
+                    $message .= '<p class="text-success text-center">'.$filename.' uploads ảnh thành công</p>'; 
                 } else {
-                    $message .= '<p class="text-danger text-center" id="notify_move">Uploads ảnh không thành công</p>';
+                    $message .= '<p class="text-danger text-center">'.$filename.' uploads ảnh không thành công</p>';
                 }
             }
         }
