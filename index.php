4124f5b9a36eff2d1d2610208b5f103c60862e3f
coolnlite <mtauto.marketing@gmail.com>
2022-06-29 16:14:32 +0700

    fix bug

--- index.php
+++ index.php
@@ -317,6 +317,58 @@
     require_once('./pages/footer.php');
     require_once('./pages/popup_form.php');
   ?>
+  <script>
+  //SLIDER 
+  var index = 0;
+  var i = 0;
+
+  var slider = $('.slider');
+  var line = $('.line');
+
+  auto();
+
+  function show(n){
+
+    for(i = 0 ; i < slider.length ; i++){
+      slider[i].style.display = "none";
+    }
+
+    for(i = 0 ; i < line.length ; i++){
+    line[i].className = line[i].className.replace("active");
+    }
+
+    slider[n-1].style.display = "block";
+    line[n-1].className += " active";
+
+  }
+
+  function auto(){
+    index++;
+    if(index > slider.length){
+      index = 1;
+    }
+    show(index);
+    setTimeout(auto,7000);
+  }
+
+  function plusSlide(n){
+    index += n;
+
+    if(index > slider.length){
+      index = 1;
+    }
+
+    if(index < 1){
+      index = slider.length;
+    }
+    show(index);
+  }
+
+  function currentSlide(n){
+    index = n;
+    show(index);
+  }
+    </script>
   <!-- javasript -->
   <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
   <script src="<?php print $base_url?>public/js/slider.js"></script>
